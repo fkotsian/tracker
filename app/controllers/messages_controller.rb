431a34100ac5fc480c0904fb1d1285ae9d31d497
Frank Kotsianas <frankkotsianas@gmail.com>
2016-05-19 23:34:18 -0700

    Create method for recording journal entries.
    - Still needs method to prompt a journal entry and expect a response on a cron job
    - Still needs method to return all responses from past week (recent entries)
    - Needs refactor of all fetching logic into TwilioMessage model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,31 @@ class MessagesController < ApplicationController
     "'Workout'"   => 'to record your workout today'
   }
   
-  def respond
+  def diary
+    body = filtered_params['Body'].strip
+    if body.empty?
+      twiml = could_not_understand
+    else
+      # saves diary message
+      m = TwilioMessage.from_params(filtered_params)
+      m.save!
+    
+      num_entries = TwilioMessage.where("created_at >= ?", beginning_of_week).count
+      # sends autoresponse: "You've diaried X days this week. To see what you did this week, respond 'Journal'. To see your personal diary, go to <name>.tracker.cfapps.io."
+    
+      twiml ||= Nokogiri::XML::Builder.new(encoding:'UTF-8') do |xml|
+        xml.Response {
+          xml.Message <<-RESP
+          You've journaled #{num_entries} days this week. Send "Journal" anytime to see your recent entries, or visit tracker.cfapps.com/your_name.
+          RESP
+        }
+      end
+      
+      render status: 200, xml: twiml
+    end
+  end
+  
+  def record
     m = TwilioMessage.from_params(filtered_params)
 
     body = filtered_params['Body']
@@ -24,14 +48,7 @@ class MessagesController < ApplicationController
       m.message_category = 'workout'
       m.save!
     else
-      twiml = Nokogiri::XML::Builder.new(encoding:'UTF-8') do |xml|
-        xml.Response {
-          xml.Message <<-RESP
-            I'm sorry, I didn't understand that.
-Try #{SUPPORTED_ACTIONS.map{|a,d| "#{a} #{d}."}.join(" ") }
-          RESP
-        }
-      end
+      twiml = could_not_understand
     end
     
     twiml ||= Nokogiri::XML::Builder.new(encoding:'UTF-8') do |xml|
@@ -55,4 +72,19 @@ Try #{SUPPORTED_ACTIONS.map{|a,d| "#{a} #{d}."}.join(" ") }
     params.permit(TwilioMessage::KNOWN_KEYS)
   end
   
+  def beginning_of_week
+    (Date.today.wday + 1).days.ago.beginning_of_day
+  end
+  
+  def could_not_understand
+    Nokogiri::XML::Builder.new(encoding:'UTF-8') do |xml|
+      xml.Response {
+        xml.Message <<-RESP
+          I'm sorry, I didn't understand that.
+Try #{SUPPORTED_ACTIONS.map{|a,d| "#{a} #{d}."}.join(" ") }
+        RESP
+      }
+    end
+  end
+    
 end
