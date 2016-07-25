ed63c8017a848022675d71ea29e795d4557f14d1
Frank Kotsianas <frankkotsianas@gmail.com>
2016-07-25 14:05:33 -0700

    Use self over classname in class methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   has_many :subscription_configurations, through: :subscriptions
 
   def self.fetch_by_phone_number(number)
-    User.joins(:phones).where(['number = ?', number]).first
+    self.joins(:phones).where(['number = ?', number]).first
   end
 end
